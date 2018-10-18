2514fe588f54ea1c3e7bed4e5f1d827e76b8554e
bnajafi <b.najafi87@gmail.com>
2018-10-18 10:05:16 +0200

    udpating

--- Assignment4/Assignment4_mrossi_example.py
+++ Assignment4/Assignment4_mrossi_example.py
@@ -1,7 +1,7 @@
 """
 This file will only include the list of resistances for the same example of 
 assignment 4, you should then import the function from WallFunctions_mrossi using both ways!
-so first you solve it using import yourfunction as nickname and another time by  : from wallFunction import yourfunctionName/s 
+so first you solve it using import wallFunction_yourName as nickname and another time by  : from wallFunction_yourName import yourfunctionName/s 
 ** use the import sys method that we learnt so that it would work also on my computer!
 
 """ 
