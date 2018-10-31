8a1c34b59d5325ecdeffb086b01fbeec345885f7
bnajafi <b.najafi87@gmail.com>
2018-10-31 15:53:23 +0100

    uodating

---
+++ Assignment6/assignment6_guidelines.py
@@ -0,0 +1,5 @@
+# In assignment 6, you are supposed to solve the same exercise
+# as assignment 4 (without importing the module, putting everything in the same file)
+# You will need to use Pandas dataFrame and to check for the resistance of materials you will have to use a function(in which you have thermalResDict) 
+# and the apply command 
+# For this assignment you have two weeks till the submission deadline
