681e67404d7b8107c3844d52f1b7c871c2fa3e0b
buketozcan <bukettozcan@gmail.com>
2018-11-21 13:29:03 +0100

    Assignment7

--- Assignment7/Assignment7_buketozcan.py
+++ Assignment7/Assignment7_buketozcan.py
@@ -30,8 +30,6 @@ IAC_cl_DF = pd.read_csv(path_file_IAC_Cl, sep=";", index_col = 1, header=0)
 IAC_cl_DF.head(3)
 IAC_cl_DF.loc["1c","BlindsDark"]
 
-
-
 def IAC_CL_finder(windowID,intShadingID):  
     name_file_IAC_Cl="IAC_cl.csv"
     path_file_IAC_Cl = os.path.join(Folder_whereThoseTablesAre,name_file_IAC_Cl) 
@@ -50,25 +48,21 @@ def IAC_CL_finder_correct(row):
     IAC_cl_value = IAC_cl_DF.loc[windowID,intShadingID]
     return IAC_cl_value
 
-
 thisWindowRow= window_DF.loc["east",:]
-
 IAC_CL_finder_correct(thisWindowRow)
-
 window_DF["IAC_cl"]=window_DF.apply(IAC_CL_finder_correct,axis=1)
-
 window_DF["IAC"]=  1.0+(window_DF["IAC_cl"]-1.0)*window_DF["IntShading_closeness"]
-
 window_DF.to_csv(path_file_modifiedwindows,sep=";")
+
 
 def Ed_finder(row):
     direction = row["Direction"]
     name_file_Ed = "DiffuseIrradiance.csv"
     path_file_Ed = os.path.join(Folder_whereThoseTablesAre,name_file_Ed)
-    Ed_DF = pd.read_csv(path_file_Ed, sep = ";", index_col = 0, header = 0)
-    name_of_columns = Ed_DF.columns.get_values()
+    DF_DiffuseIrradiance = pd.read_csv(path_file_Ed, sep = ";", index_col = 0, header = 0)
+    name_of_columns = DF_DiffuseIrradiance.columns.get_values()
     name_of_columns_as_numbers = name_of_columns.astype(np.int32, copy=False)
-    Ed_Value = sp.interp(latitude,name_of_columns_as_numbers,Ed_DF.loc[direction])
+    Ed_Value = sp.interp(latitude,name_of_columns_as_numbers,DF_DiffuseIrradiance.loc[direction])
     return Ed_Value                
 
 window_DF["Ed"] = window_DF.apply(Ed_finder,axis=1)
@@ -77,10 +71,10 @@ def ED_finder(row):
     direction = row["Direction"]
     name_file_ED = "BeamIrradiance.csv"
     path_file_ED = os.path.join(Folder_whereThoseTablesAre,name_file_ED)
-    ED_DF = pd.read_csv(path_file_ED, sep = ";", index_col = 0, header = 0)
-    name_of_columns = ED_DF.columns.get_values()
+    DF_BeamIrradiance = pd.read_csv(path_file_ED, sep = ";", index_col = 0, header = 0)
+    name_of_columns = DF_BeamIrradiance.columns.get_values()
     name_of_columns_as_numbers = name_of_columns.astype(np.int32, copy=False)
-    ED_Value = sp.interp(latitude,name_of_columns_as_numbers,ED_DF.loc[direction])    
+    ED_Value = sp.interp(latitude,name_of_columns_as_numbers,DF_BeamIrradiance.loc[direction])    
     return ED_Value         
 
 window_DF["ED"] = window_DF.apply(ED_finder,axis=1)
