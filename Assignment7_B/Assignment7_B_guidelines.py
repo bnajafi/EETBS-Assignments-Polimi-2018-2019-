1bec396332dea1ead363617da281b057a40fa5df
bnajafi <b.najafi87@gmail.com>
2018-11-25 17:07:52 +0100

    updating

--- Assignment7_B/Assignment7_B_guidelines.py
+++ Assignment7_B/Assignment7_B_guidelines.py
@@ -1,3 +1,3 @@
 #♣  In this assignment you should use Jupyter notebooks and paste all of the code
 #we wrote for plotting the time series data for the dates (15th of May 2014 till 30th of May 2014" and create 3 different subplots that show the consumption, temperature, irradiance
-# and finally plot them together after normalizing 3 of them 
+# and finally plot them together after normalizing 3 of them *
